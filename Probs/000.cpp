4523caffb9de9fb28526e5385411f9ebc4ec2750
iitjee <iit.jee.help.me@gmail.com>
2017-01-24 23:44:37 +0530

    Update 000.cpp

--- Probs/000.cpp
+++ Probs/000.cpp
@@ -6,5 +6,6 @@ GrafixMask: See sol,
   - uses head&tail as another way of looking at stack
   - q[tail++]=y*1000+x it seems like some row-major order but 1000 is taken just so that it's >400, 600 and it can be parsed into x and y later. Fancy way of storing
   - 
+  - DFS using nodes (ctrlF for 'grafix' http://se7so.blogspot.in/2012/04/problem-solving-techniques-example_19.html )
   - )
 https://community.topcoder.com/stat?c=problem_statement&pm=2998&rd=5857
