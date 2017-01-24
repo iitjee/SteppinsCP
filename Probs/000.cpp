44d85faf116f779ba848922f2e53bb76a8b07f76
iitjee <iit.jee.help.me@gmail.com>
2017-01-24 22:50:32 +0530

    Create 000.cpp

---
+++ Probs/000.cpp
@@ -0,0 +1,10 @@
+
+Graph Theory:
+
+GrafixMask: See sol, 
+( - The sol is based on DFS.
+  - uses head&tail as another way of looking at stack
+  - q[tail++]=y*1000+x it seems like some row-major order but 1000 is taken just so that it's >400, 600 and it can be parsed into x and y later. Fancy way of storing
+  - 
+  - )
+https://community.topcoder.com/stat?c=problem_statement&pm=2998&rd=5857
