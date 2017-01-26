73a7c9d6ec9bdbe9423e63f27fbb80c906c92679
iitjee <iit.jee.help.me@gmail.com>
2017-01-27 01:27:47 +0530

    Create GreedyNotes.cpp

---
+++ DataStructures/GreedyNotes.cpp
@@ -0,0 +1,18 @@
+
+
+
+
+problems which can be solved by DP, in many cases a Greedy strategy could also be employed.
+https://www.topcoder.com/community/data-science/data-science-tutorials/greedy-is-good/
+BioScore: https://community.topcoder.com/stat?c=problem_statement&pm=3038
+          https://apps.topcoder.com/forums/?module=Thread&threadID=760642&start=0&mc=2#2126909
+
+
+
+
+
+
+
+
+
+
