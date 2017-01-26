844924025310068a91a9165caf7a63d6e89c7b1c
iitjee <iit.jee.help.me@gmail.com>
2017-01-26 11:39:41 +0530

    Create datatypes.cpp

---
+++ CPPLanguage/datatypes.cpp
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+Signed and Unsigned:
+
+unsigned is a shorthand for unsigned int, and so defined in standard C
+struct Stack
+{
+    int top;
+    unsigned capacity;
+    int* array;
+};
+
+
+
+
+
+
+
+
