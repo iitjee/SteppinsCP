6c86cbb7faacc767f7e184dfb834ffc0c7f2e729
iitjee <iit.jee.help.me@gmail.com>
2017-10-07 18:03:52 +0530

    Update Pointers3 Comparisons.cpp

--- CPPLanguage/Pointers3 Comparisons.cpp
+++ CPPLanguage/Pointers3 Comparisons.cpp
@@ -10,24 +10,28 @@ only if       they are both null,
 
 
 **Relational operators (<,>,<=,>=)**
-Pointers to objects or functions of the same type (after pointer conversions) can be compared, with a result defined as follows:
+Pointers to objects or functions of the same type (after pointer conversions) can be compared, with a result defined as 
+follows:
 
-If two pointers p and q of the same type point to the same object or function, or both point one past the end of the same array, or are 
-both null, then p<=q and p>=q both yield true and p<q and p>q both yield false.
+If two pointers p and q of the same type point to the same object or function, or both point one past the end of the same 
+array, or are both null, then p<=q and p>=q both yield true and p<q and p>q both yield false.
 
-If two pointers p and q of the same type point to different objects that are not members of the same object or elements of the same 
-array or to different functions, or if only one of them is null, the results of p<q, p>q, p<=q, and  p>=q are unspecified.
+If two pointers p and q of the same type point to different objects that are not members of the same object or elements of the 
+same array or to different functions, or if only one of them is null, the results of p<q, p>q, p<=q, and  p>=q are 
+unspecified.
 
-If two pointers point to non-static data members of the same object, or to subobjects or array elements of such members, recursively, 
-the pointer to the later declared member compares greater provided the two members have the same access control (Clause 11) and provided 
-their class is not a union.
+If two pointers point to non-static data members of the same object, or to subobjects or array elements of such members, 
+recursively, the pointer to the later declared member compares greater provided the two members have the same access control 
+(Clause 11) and provided their class is not a union.
 
-If two pointers point to non-static data members of the same object with different access control (Clause 11) the result is unspecified.
+If two pointers point to non-static data members of the same object with different access control (Clause 11) the result is 
+unspecified.
 
-If two pointers point to non-static data members of the same union object, they compare equal (after conversion to void*, if necessary). 
+If two pointers point to non-static data members of the same union object, they compare equal (after conversion to void*, if 
+necessary). 
 
-IMP: If two pointers point to elements of the same array or one beyond the end of the array, the pointer to the object with the higher 
-subscript compares higher.
+IMP: If two pointers point to elements of the same array or one beyond the end of the array, the pointer to the object with 
+the higher subscript compares higher.
 
 Other pointer comparisons are unspecified.
 
