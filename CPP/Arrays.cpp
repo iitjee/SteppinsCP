7acc0010bfa592142b72ec59b5470a45208703d3
iitjee <iit.jee.help.me@gmail.com>
2017-12-04 17:20:14 +0530

    Update Arrays.cpp

--- CPP/Arrays.cpp
+++ CPP/Arrays.cpp
@@ -60,12 +60,9 @@ int main()
  
     // Print the table
     for (int row = 1; row < numRows; ++row)
-    {
         for (int col = 1; col < numCols; ++col)
             std::cout << product[row][col] << "\t";
- 
-        std::cout << '\n';
-    }
+
  
     return 0;
 }
